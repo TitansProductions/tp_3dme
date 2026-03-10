9a7301709ec236812b8fe2ce9eab1442ad5328a4
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2026-03-11 00:31:44 +0200

    Create config.lua

---
+++ tp_3dme/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+-- The command to use for displaying the 3DText.
+Config.Command = "me"
+Config.CommandSuggestion = 'Display a text above your player head.'
+Config.CommandActionText = "What text you want to display?"
+
+-- The RGBA color when the text is displayed.
+Config.Color = { r = 255, g = 215, b = 0, a = 200 } -- Text color
+
+-- How long should the displayed text last in milliseconds.
+Config.Duration = 5000
+
+-- The maximum distance to display to nearby players.
+Config.DisplayDistance = 20.0
