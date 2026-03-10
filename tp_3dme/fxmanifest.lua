d05384157e5eb28af0b076746496b697230f8047
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2026-03-11 00:31:03 +0200

    Create fxmanifest.lua

---
+++ tp_3dme/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "adamant"
+games {"rdr3"}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'Nosmakos'
+description 'Titans Productions - 3DME'
+version '1.0.0'
+
+shared_scripts { 'config.lua' }
+server_scripts { 'server/main.lua' }
+client_scripts { 'client/*.lua' }
+
+dependencies {
+    'tp_libs',
+}
+
+lua54 'yes'
